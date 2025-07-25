87b2dc70c9a76dbf04b8982c9d40a4bb9bcbdf6b
Usmaan Rifkhan <31176872+usmaan9990@users.noreply.github.com>
2025-07-25 14:45:02 +0530

    Update pendingstreamlit_app.py
    changing SiS to SniS theer for seession change

--- pendingstreamlit_app.py
+++ pendingstreamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app 
@@ -10,7 +9,9 @@ st.write(
   Orders that need to be filled
   """)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
