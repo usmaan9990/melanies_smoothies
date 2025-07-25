5ed807745c0c8bf51e0dd4d8fa6949629ae3200e
Usmaan Rifkhan <31176872+usmaan9990@users.noreply.github.com>
2025-07-25 17:22:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 
+# snowpark dataframe converting to pandas so we can use loc
 pd_df = my_dataframe.to_pandas()
 #st.dataframe(pd_df)
 #st.stop()
@@ -33,15 +34,9 @@ ingredients_list = st.multiselect(
 
 
 if ingredients_list:
-  
-  
-  
   ingredients_string =''
   for fruit_chosen in ingredients_list:
-    
-  
     ingredients_string += fruit_chosen + ' '
-
     # will get the value from searchon and tell for x value search keyword is y
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
     st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -51,12 +46,10 @@ if ingredients_list:
     # st.text(smoothiefroot_response.json())
     sf_df = st.dataframe(smoothiefroot_response.json(), use_container_width=True)
       
-
   st.write(ingredients_string)
   # now will insert this to order table
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
           values ('""" + ingredients_string + """','""" + name_on_order +"""')"""
-
   st.write(my_insert_stmt)
 
   # will insert submmit btn; this will avoid feeding each and 
@@ -64,8 +57,6 @@ if ingredients_list:
   time_to_insert = st.button("Submit Order")
 
   if time_to_insert:
-    
-    
     session.sql(my_insert_stmt).collect()
     st.success(f"Your Smoothie is ordered!, {name_on_order}!", icon="✅")
 
