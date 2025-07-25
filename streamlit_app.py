fee47b03b3defab401a1b0841b0deda3de06efec
Usmaan Rifkhan <31176872+usmaan9990@users.noreply.github.com>
2025-07-25 23:51:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,11 @@ if ingredients_list:
     ingredients_string += fruit_chosen + ' '
     # will get the value from searchon and tell for x value search keyword is y
     search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON']
-        if pd.isna(search_on.iloc[0]):
-          search_on = search_on.fillna(fruit_chosen).iloc[0]
-        else:
-          search_on = search_on.iloc[0]
+    
+    if pd.isna(search_on.iloc[0]):
+      search_on = search_on.fillna(fruit_chosen).iloc[0]
+    else:
+      search_on = search_on.iloc[0]
     
     st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
