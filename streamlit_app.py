eaa3ef96bb1aa02d0e2d7c17c75784598b12ef03
Usmaan Rifkhan <31176872+usmaan9990@users.noreply.github.com>
2025-07-25 17:05:29 +0530

    Update streamlit_app.py
    ex; the apples keyword for that is apple

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,13 @@ if ingredients_list:
     ingredients_string =''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        # will get the value from searchon and tell for x value search keyword is y
+        search_on = pd_df.loc[pd_df.loc["FRUIT_NAME"] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+
+
+      
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
         # st.text(smoothiefroot_response.json())
